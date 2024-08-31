7ed64d228f7839063460007badcb07880ad0e81d
Pedro Henrique <pedrohenriqueb366@gmail.com>
2024-08-31 11:48:38 -0300

    feat: validator login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export default class LoginComponent {
+export default class LoginComponent{
+
+  fb = inject(FormBuilder)
+  form = this.fb.group({
+    login: ['', [Validators.required, Validators.minLength(1)]],
+  })
 
 }
